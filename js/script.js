fdbc69ead55df1ca8d575c97c58cb3c96850a834
yurilov <yuraschepilov@gmail.com>
2022-01-14 16:12:28 +0200

    added simple clicker

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
-console.log('hello!')
+window.onload = function () {
+  const jumpRef = document.querySelector('#jump');
+  let count = 1;
+  const scoreRef = document.querySelector('.score');
+  jumpRef.addEventListener('click', () => {
+    count += 1;
+    scoreRef.textContent = count;
+  });
+};
