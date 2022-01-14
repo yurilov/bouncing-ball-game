d814d11ca85906928f20aa46bd9955616906944a
yurilov <yuraschepilov@gmail.com>
2022-01-14 16:41:35 +0200

    added canvas

--- js/script.js
+++ js/script.js
@@ -2,6 +2,11 @@ window.onload = function () {
   const jumpRef = document.querySelector('#jump');
   let count = 1;
   const scoreRef = document.querySelector('.score');
+  const canvas = document.getElementById("canvas");
+  const context = canvas.getContext("2d");
+  context.moveTo(150, 100);
+  context.lineTo(600, 400);
+  context.stroke();
   jumpRef.addEventListener('click', () => {
     count += 1;
     scoreRef.textContent = count;
