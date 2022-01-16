5663bcb0497860fae9c22ae2cbf32ce00315c63a
Yurii Shchepilov <yuraschepilov@gmail.com>
2022-01-16 10:51:46 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,13 +1,17 @@
 window.onload = function () {
-  const jumpRef = document.querySelector('#jump');
+  const jumpRef = document.querySelector("#jump");
   let count = 1;
-  const scoreRef = document.querySelector('.score');
+  const scoreRef = document.querySelector(".score");
+
   const canvas = document.getElementById("canvas");
   const context = canvas.getContext("2d");
-  context.moveTo(150, 100);
-  context.lineTo(600, 400);
+  let x = 300;
+  let y = 350;
+  context.arc(x, y, 50, 0, 2 * Math.PI);
+  context.fillStyle = "red";
+  context.fill();
   context.stroke();
-  jumpRef.addEventListener('click', () => {
+  jumpRef.addEventListener("click", () => {
     count += 1;
     scoreRef.textContent = count;
   });
