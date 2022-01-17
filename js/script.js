597da4465e5c56e4c160063e2efca2cccbe0dd6c
Yurii Shchepilov <89077590+yurilov@users.noreply.github.com>
2022-01-17 15:51:39 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,18 +1,31 @@
 window.onload = function () {
-  const jumpRef = document.querySelector("#jump");
+  const jumpRef = document.querySelector('#jump');
   let count = 1;
-  const scoreRef = document.querySelector(".score");
 
-  const canvas = document.getElementById("canvas");
-  const context = canvas.getContext("2d");
+  const canvas = document.getElementById('canvas');
+  const context = canvas.getContext('2d');
   let x = 300;
   let y = 350;
   context.arc(x, y, 50, 0, 2 * Math.PI);
-  context.fillStyle = "red";
+  context.fillStyle = 'red';
   context.fill();
   context.stroke();
-  jumpRef.addEventListener("click", () => {
+  jumpRef.addEventListener('click', () => {
     count += 1;
-    scoreRef.textContent = count;
+    if (y <= 0) {
+      y = 425;
+    } else {
+      y -= 25;
+    }
+
+    context.clearRect(0, 0, 600, 400);
+
+    context.beginPath();
+    context.arc(x, y, 50, 0, 2 * Math.PI);
+    context.fillStyle = 'red';
+    context.fill();
+    context.font = '25px Arial';
+    context.fillStyle = 'black';
+    context.fillText('Count: ' + count, 20, 30);
   });
 };
