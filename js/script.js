61741270636066b7b2458347bde14946a3e0de61
yurilov <yuraschepilov@gmail.com>
2022-01-20 15:33:28 +0200

    1 file modified

--- js/script.js
+++ js/script.js
@@ -10,31 +10,32 @@ window.onload = function () {
   context.fillStyle = 'red';
   context.fill();
   context.stroke();
-  jumpRef.addEventListener('click', jumpClickHandler);
-
-  function jumpClickHandler() {
+  jumpRef.addEventListener('click', () => {
     count += 1;
     if (y <= 0) {
       y = 425;
     } else {
       y -= 25;
     }
-  
-    ​window.requestAnimationFrame(draw);
-  }
 
-  function draw() {
-    context.clearRect(0, 0, 600, 400);
-  
-    context.beginPath();
-    context.arc(x, y, 50, 0, 2 * Math.PI);
-    context.fillStyle = 'red';
-    context.fill();
-    context.font = '25px Arial';
-    context.fillStyle = 'white';
-    context.fillText('Count: ' + count, 20, 30);
-  }
+    draw();
+
+    function draw() ​{
+
+      ​context.clearRect(0, 0, 600, 400);
+
+      ​context.beginPath();
+      ​context.arc(x, y, 50, 0, 2 * Math.PI);
+      ​context.fillStyle="red";
+      ​context.fill();
+
+      ​context.font = '25px Arial';
+      ​context.fillStyle = 'white';
+      ​context.fillText("Count: " + count, 20, 30);
+
+      ​window.requestAnimationFrame(draw);
+    }
+    
+  });
 };
-
-
 
